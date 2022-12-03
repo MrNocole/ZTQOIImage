0e84077e66d228567cd2e6b561a46927ab601761
= <1926276913@qq.com>
2022-12-03 21:02:17 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-ZTQOIImage_Example/Pods-ZTQOIImage_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZTQOIImage_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ZTQOIImage_Example
+@end
---
+++ Example/Pods/Target Support Files/ZTQOIImage/ZTQOIImage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZTQOIImage : NSObject
+@end
+@implementation PodsDummy_ZTQOIImage
+@end
